f939c79f8a32b4907612e3c029195bc3224cd60c
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 20:50:18 +0400

    add HTML, CSS for UiInput

---
+++ components/UI/UiInput.vue
@@ -0,0 +1,21 @@
+<template>
+	<input
+		type="text"
+		class="input"
+		placeholder="Поиск по объявлениям"
+	/>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.input {
+	background: #ffffff;
+	border-radius: 5px;
+	padding: 14px 21px;
+	width: 100%;
+	border: 3px solid #256eeb;
+}
+</style>
