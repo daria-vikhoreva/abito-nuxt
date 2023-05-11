db5d1460eb05902497b775fc2adeb2ead3081eeb
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 22:06:11 +0400

    add HTML, CSS for AppFooter

---
+++ components/AppFooter.vue
@@ -0,0 +1,45 @@
+<template>
+	<footer class="footer">
+		<div class="footer__border"></div>
+		<div class="footer__info">
+			<div class="footer__company">© ООО «Абито», 2011–2021</div>
+			<a
+				href="#"
+				class="footer__link"
+				>Политика конфиденциальности</a
+			>
+			<a
+				href="#"
+				class="footer__link"
+				>Обработка данных</a
+			>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+	display: flex;
+	flex-direction: column;
+}
+.footer__border {
+	margin-top: 37px;
+	border: 1px solid #e5e5e5;
+}
+.footer__info {
+	margin-top: 18px;
+	font-size: 14px;
+}
+.footer__company {
+	color: #c4c4c4;
+}
+.footer__link {
+	margin-top: 5px;
+	color: #c4c4c4;
+	display: block;
+}
+</style>
