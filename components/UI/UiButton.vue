e0d8fa17498172914f3c03c5dfbe02801d9ed081
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 18:31:07 +0400

    added HTML and CSS for UiButton

---
+++ components/UI/UiButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<button class="button">{{ text }}</button>
+</template>
+
+<script setup>
+const props = defineProps({
+	text: String,
+});
+</script>
+
+<style scoped>
+.button {
+	padding: 10px 15px;
+	background: #256eeb;
+	border-radius: 5px;
+	font-size: 14px;
+	line-height: 16px;
+	color: #ffffff;
+}
+</style>
