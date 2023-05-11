eae4bb971575c09030e3f9847d1960b4052aa205
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 20:49:01 +0400

    update logic and styles for UiButton

--- components/UI/UiButton.vue
+++ components/UI/UiButton.vue
@@ -1,20 +1,44 @@
 <template>
-	<button class="button">{{ text }}</button>
+	<button
+		class="button"
+		:class="size"
+	>
+		<slot name="left" />
+		{{ text }}
+		<slot name="right" />
+	</button>
 </template>
 
-<script setup>
-const props = defineProps({
-	text: String,
+<script setup lang="ts">
+interface Props {
+	text: string;
+	size?: 'small' | 'medium' | 'large';
+}
+
+withDefaults(defineProps<Props>(), {
+	size: 'large',
 });
 </script>
 
 <style scoped>
 .button {
-	padding: 10px 15px;
 	background: #256eeb;
 	border-radius: 5px;
 	font-size: 14px;
 	line-height: 16px;
 	color: #ffffff;
+	border: 0;
+}
+
+.small {
+	padding: 10px 15px;
+}
+
+.medium {
+	padding: 16px 20px 17px 20px;
+}
+
+.large {
+	padding: 19px 62px;
 }
 </style>
