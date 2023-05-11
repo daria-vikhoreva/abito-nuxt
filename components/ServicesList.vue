62af7c7377dea638e4ddeb18ac28bceddb77bebc
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 22:05:28 +0400

    add HTML, CSS for ServicesList and ServicesListItem

---
+++ components/ServicesList.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="services-list">
+		<h2 class="services-list__title">Сервисы и услуги</h2>
+		<div class="services-list__wrapper">
+			<ServicesListItem />
+			<ServicesListItem />
+			<ServicesListItem />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.services-list__wrapper {
+	margin-top: 29px;
+	display: flex;
+	flex-direction: column;
+	gap: 25px;
+}
+.services-list__title {
+	font-weight: 700;
+	font-size: 18px;
+	line-height: 24px;
+}
+</style>
---
+++ components/ServicesListItem.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="service">
+		<div class="service__img">
+			<img
+				src="~/assets/delivery.svg"
+				alt="img"
+			/>
+			<div class="service__title">Доставка</div>
+			<div class="service__descr">
+				Проверка при получении и возможность бесплатно вернуть товар
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.service__title {
+	font-weight: 700;
+	font-size: 14px;
+	line-height: 16px;
+}
+</style>
