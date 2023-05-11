07bbfde7bb5ab47ec51e647559b8c2904e4844d7
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 18:31:31 +0400

    added HTML and CSS for UiLink

---
+++ components/UI/UiLink.vue
@@ -0,0 +1,22 @@
+<template>
+	<a
+		:href="link.href"
+		class="link"
+		>{{ link.name }}</a
+	>
+</template>
+
+<script setup>
+const props = defineProps({
+	link: Object,
+});
+</script>
+
+<style scoped>
+.link {
+	text-decoration: none;
+	color: #0a143a;
+	font-size: 14px;
+	line-height: 16px;
+}
+</style>
