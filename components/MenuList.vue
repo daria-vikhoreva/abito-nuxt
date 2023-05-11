c709a1d9daba8dfabe780078ec9d0569c0a49451
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-05-11 18:32:55 +0400

    added HTML and CSS for MenuList

---
+++ components/MenuList.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="menu">
+		<UiLink
+			v-for="(item, index) in menu"
+			:key="index"
+			class="menu__item"
+			:link="item"
+		/>
+	</div>
+</template>
+
+<script setup>
+const menu = [
+	{ name: 'Авто', href: '#' },
+	{ name: 'Недвижимость', href: '#' },
+	{ name: 'Работа', href: '#' },
+	{ name: 'Услуги', href: '#' },
+];
+</script>
+
+<style scoped>
+.menu {
+	display: flex;
+	gap: 14px;
+}
+</style>
